10a3450c6c96d210a994f3470dfaf97734defc87
Gustavo Carneiro <gustavocarneiro.a@gmail.com>
2024-08-28 19:57:38 -0300

    adding schema validators to rest api

--- learn_fastapi/app.py
+++ learn_fastapi/app.py
@@ -1,8 +1,47 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+
+from learn_fastapi.schemas import IndividualSchema, IndividualSchemaDTO
 
 app = FastAPI()
 
+Database: list[IndividualSchema] = [
+    IndividualSchema(
+        id=1,
+        name='Gustavo',
+        email='gusta@vo.carneiro',
+        document='cpf',
+        document_id='06124721589791',
+    ),
+    IndividualSchema(
+        id=2,
+        name='Gustavo',
+        email='gusta@vo.carneiro',
+        document='cpf',
+        document_id='06124721589791',
+    ),
+]
 
-@app.get('/')
-def read_root():
-    return {'message': 'Hello World@'}
+
+@app.get('/', response_model=list[IndividualSchema])
+def get_all_users():
+    return Database
+
+
+@app.get('/{id}', response_model=IndividualSchema)
+def get_user(id: int):
+    if id > len(Database) or id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='user_not_found'
+        )
+
+    return Database[id - 1]
+
+
+@app.post('/', status_code=HTTPStatus.CREATED, response_model=IndividualSchema)
+def create_user(individualDTO: IndividualSchemaDTO):
+    user = IndividualSchema(**individualDTO.model_dump(), id=len(Database) + 1)
+    Database.append(user)
+
+    return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,65 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
+from learn_fastapi.schemas import IndividualSchema
 
-from learn_fastapi.app import app
+test_init_database_reponse = [
+    IndividualSchema(
+        id=1,
+        name='Gustavo',
+        email='gusta@vo.carneiro',
+        document='cpf',
+        document_id='06124721589791',
+    ).__dict__,
+    IndividualSchema(
+        id=2,
+        name='Gustavo',
+        email='gusta@vo.carneiro',
+        document='cpf',
+        document_id='06124721589791',
+    ).__dict__,
+]
 
 
-def test_read_root_should_return_ok_and_hello_world():
-    client = TestClient(app)
+def test_should_get_all_clients(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Hello World@'}
+    assert response.json() == test_init_database_reponse
+
+
+def test_should_create_a_client(client):
+    body = {
+        'name': 'gustavo',
+        'email': 'gus@ta.vo',
+        'document': 'cpf',
+        'document_id': '06100000000',
+    }
+    response = client.post('/', json=body)
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {**body, 'id': 3}
+
+    body = {
+        'name': 'gustavo',
+        'email': 'gus@ta.vo',
+        'document': 'cpf',
+        'document_id': '06100000000',
+    }
+    response = client.post('/', json=body)
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {**body, 'id': 4}
+
+
+def test_should_retrieve_a_client(client):
+    response = client.get('/2')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == test_init_database_reponse[1]
+
+
+def test_should_retrieve_an_error_when_invalid_client_id(client):
+    response = client.get('/20')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'user_not_found'}
